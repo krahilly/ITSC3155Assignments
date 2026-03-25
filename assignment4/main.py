56f3d3a94ae8fa4a9133c15dfb596e0ed6d4bf7a
krahilly <krahilly@charlotte.edu>
2026-03-24 23:48:27 -0400

    Update main.py
    feat: added pydantic and a new class

--- assignment4/main.py
+++ assignment4/main.py
@@ -1,5 +1,11 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 app = FastAPI()
 
